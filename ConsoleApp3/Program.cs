9a63e49712ae62509fea7fc94a5c3d2c36bb05f3
ABK <ABK@DESKTOP-PTCS16K>
2024-12-23 11:45:08 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1 && num<= 1000)
+{
+    Console.WriteLine((num * (num + 1)) / 2);
+}
+
+Console.WriteLine("------------------");
+string str = Console.ReadLine();
+string vowels = "aeiou";
+int i = 0;
+int sum = 0;
+foreach (char c in str.ToLower())
+{
+    if (vowels.Contains(c))
+    { sum+=1; }
+    }
+Console.WriteLine(sum);
+
+Console.WriteLine("---------------");
+int num1= Convert.ToInt32(Console.ReadLine());
+if (num1 %2==0)
+{
+    Console.WriteLine("true");
+}else {  Console.WriteLine("false"); }
+
+Console.WriteLine("-----------------------");
+int factorial;
+int num2 = Console.ReadLine();
+for (int i = 0; i <= num2; i++)
+{
+    factorial *= num2
+}
+
